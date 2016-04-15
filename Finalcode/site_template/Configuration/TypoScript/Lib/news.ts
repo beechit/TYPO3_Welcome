fead7de0378508bea94eb98568afd02e656df33a
Ruud Silvrants <ruud@beech.it>
2016-04-15 11:21:39 +0200

    [TASK] Add code of news override template

---
+++ Finalcode/site_template/Configuration/TypoScript/Lib/news.ts
@@ -0,0 +1,15 @@
+plugin.tx_news {
+    view {
+        templateRootPaths {
+            102 = EXT:site_template/Resources/Private/Ext/News/Templates/
+        }
+
+        partialRootPaths {
+            102 = EXT:site_template/Resources/Private/Ext/News/Partials/
+        }
+
+        layoutRootPaths {
+            102 = EXT:site_template/Resources/Private/Ext/News/Layouts/
+        }
+    }
+}
